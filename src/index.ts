039bd6f11f0cc7d25795dc54827f1502adeeb42d
João Victor Davi de Oliveira <j.victordavid2@gmail.com>
2025-04-12 13:29:56 -0300

    refatora função main para otimizar download e exclusão de arquivos, melhorando a gestão de promessas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { createWriteStream } from 'fs'
 import { mkdir } from 'fs/promises'
 import { Client } from 'minio'
-import {
-  dirname
-} from 'path'
+import { dirname } from 'path'
 import type { ObjectInfo } from './types'
 
 const client = new Client({
@@ -14,63 +12,83 @@ const client = new Client({
   secretKey: process.env.MINIO_SECRET_KEY
 })
 
-async function main() {
-  const bucketName = 'public'
-  const folder = process.env.MINIO_FOLDER || 'verification/broker'
-  const folderName = folder
-  const savePath = 'downloads/' + folder.split('/').pop() + '/'
+const bucketName = 'public'
+const folder = process.env.MINIO_FOLDER || 'verification/broker'
+const folderName = folder
+const savePath = 'downloads/' + folder.split('/').pop() + '/'
 
+async function objDownload(obj: ObjectInfo) {
+  if (obj.name == null) return
+  const fullPath = `${savePath}${obj.name.replace(folderName, '')}`
+  const dir = dirname(fullPath)
+  try {
+    const stat = await mkdir(dir, { recursive: true })
+    if (stat) {
+      console.log(`Created directory: ${dir}`)
+    }
+  } catch (err: any) {
+    if (err.code !== 'EEXIST') {
+      console.error(`Error creating directory: ${dir}`, err)
+    }
+  }
+  const fileStream = await client.getObject(bucketName, obj.name)
+  const write = createWriteStream(fullPath, {
+    flags: 'w',
+    encoding: 'binary'
+  })
+  fileStream.pipe(write)
+  await new Promise<void>((resolve, reject) => {
+    write.on('finish', resolve)
+    write.on('error', reject)
+  })
+}
+
+async function objDelete(obj: ObjectInfo) {
+  if (obj.name == null) return
+  await client.removeObject(bucketName, obj.name)
+}
+
+async function main() {
   const files = await client.listObjects(bucketName, folderName, true)
-  const fileList: ObjectInfo[] = []
   console.log('Fetching file list...')
+  let filesDownloaded = 0
+  let count = 0
+  const promises: Promise<void>[] = []
   await new Promise(resolve => {
-    let count = 0
     files.on('data', obj => {
-      fileList.push(obj)
       count++
       if (count % 100 === 0) {
         console.log(`Fetched ${count} files...`)
       }
+      const promise = objDownload(obj)
+          .then(() => {
+            filesDownloaded++
+            if (filesDownloaded % 100 === 0) {
+              console.log(`Downloaded ${filesDownloaded} files...`)
+            }
+            return objDelete(obj)
+              .then(() => {
+                console.log(`Deleted ${obj.name}`)
+              })
+              .catch(err => {
+                console.error(`Error deleting ${obj.name}:`, err)
+              })
+          })
+          .catch(err => {
+            console.error('Error downloading file:', err)
+          }).finally(() => {
+            promises.splice(promises.indexOf(promise), 1)
+          })
+      promises.push(
+        promise
+      )
     })
     files.on('end', () => {
       resolve(null)
     })
   })
-  const fileCount = fileList.length
-  console.log(`Total files: ${fileCount}`)
-  let filesDownloaded = 0
-  const promises = fileList.map(async objInfo => {
-    if (objInfo.name == null) return
-    const fullPath = `${savePath}${objInfo.name.replace(folderName, '')}`
-    const dir = dirname(fullPath)
-    try {
-      const stat = await mkdir(dir, { recursive: true })
-      if (stat) {
-        console.log(`Created directory: ${dir}`)
-      }
-    } catch (err: any) {
-      if (err.code !== 'EEXIST') {
-        console.error(`Error creating directory: ${dir}`, err)
-      }
-    }
-    console.log(`Downloading ${objInfo.name} to ${fullPath}`)
-    const fileStream = await client.getObject(bucketName, objInfo.name)
-    const write = createWriteStream(fullPath, {
-      flags: 'w',
-      encoding: 'binary'
-    })
-    fileStream.pipe(write)
-    await new Promise<void>((resolve, reject) => {
-      write.on('finish', resolve)
-      write.on('error', reject)
-    })
-    filesDownloaded++
-    console.log(`Downloaded ${filesDownloaded} of ${fileCount}: ${objInfo.name}`)
-    await client.removeObject(bucketName, objInfo.name)
-    console.log(`Deleted ${objInfo.name}`)
-  })
   await Promise.all(promises)
-  console.log(`Downloaded ${filesDownloaded} of ${fileCount} files`)
+  console.log(`Downloaded ${filesDownloaded} of ${count} files`)
   console.log('All files downloaded')
   console.log('Done')
 }
